497e65414a788ba6c260a746e923128eb638f162
Karthick.T <tkarthick085@gmail.com>
2026-03-19 17:50:19 +0530

    added new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,14 +3,14 @@ export const projects = [
     id: 1,
     title: "Todo App",
     description: "A simple React todo app with local storage.",
-    image: "/public/Todo.jpg",
+    image: "/images/Todo.jpg",
     liveLink: "https://todo-app-using-react-seven-xi.vercel.app/",
   },
   {
     id: 2,
     title: "Portfolio Website",
     description: "My personal portfolio made with React and Vite.",
-    image: "/public/Weather.png",
+    image: "/images/Weather.png",
     liveLink: "https://my-portfolio-website-five-sage.vercel.app/",
   },
   
