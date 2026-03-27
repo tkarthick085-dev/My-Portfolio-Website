3e5115a89dcfdc5ae6f33e945a4a31380b8ebff3
Karthick.T <tkarthick085@gmail.com>
2026-03-27 09:31:52 +0530

    Updated index.css styles

--- eslint.config.js
+++ eslint.config.js
@@ -5,26 +5,42 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    // Global ignores must be in their own object or use globalIgnores utility
+    ignores: ['dist'], 
+  },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
-    'no-unused-vars': ['warn', { 
-  varsIgnorePattern: '^[A-Z_]',
-  argsIgnorePattern: '^_' 
-}],
+    rules: {
+      // Base recommended rules
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      // Your custom override
+      'no-unused-vars': ['warn', { 
+        varsIgnorePattern: '^[A-Z_]', 
+        argsIgnorePattern: '^_' 
+      }],
+      
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
 ])
